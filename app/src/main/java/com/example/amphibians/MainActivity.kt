0b1fc2f18641843dbdb249fccdeda8c5641c99b1
alfian011203 <136213724+alfian011203@users.noreply.github.com>
2024-12-12 21:49:10 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/amphibians/MainActivity.kt
+++ app/src/main/java/com/example/amphibians/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.amphibians
 
 import android.os.Bundle
@@ -26,16 +10,21 @@ import androidx.compose.ui.Modifier
 import com.example.amphibians.ui.AmphibiansApp
 import com.example.amphibians.ui.theme.AmphibiansTheme
 
+// TODO 1: MainActivity adalah entry point aplikasi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // TODO 2: Tetapkan konten untuk aplikasi menggunakan Jetpack Compose
         setContent {
+            // TODO 3: Bungkus UI dalam tema khusus aplikasi (AmphibiansTheme)
             AmphibiansTheme {
-                // A surface container using the 'background' color from the theme
+                // TODO 4: Gunakan Surface sebagai container utama untuk elemen UI
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), // Memenuhi layar
+                    color = MaterialTheme.colorScheme.background // Warna latar dari tema
                 ) {
+                    // TODO 5: Panggil fungsi utama aplikasi (AmphibiansApp) untuk memuat UI dan data
                     AmphibiansApp()
                 }
             }
